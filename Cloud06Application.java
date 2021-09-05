0cddfd99af1bf90065fa6a0d2e0aab1f464a2add
BrunaCorty <86329180+BrunaCorty@users.noreply.github.com>
2021-09-05 19:41:35 -0300

    Add files via upload

---
+++ Cloud06Application.java
@@ -0,0 +1,13 @@
+package com.cloudtreino.cloud06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloud06Application.class, args);
+	}
+
+}
---
+++ Cloud06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtreino.cloud06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
